c1cdd04c44b160c6d299d1ecdcab73551d8efc5a
tasialedet <87391755+tasialedet@users.noreply.github.com>
2021-12-06 16:37:55 -0600

    Started webpage with bio section, image section, added Video element

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import logoGif from "./moneyrooklogo.gif";
+import imageOne from "./IMG_3715.jpeg";
+import imageTwo from "./IMG_3718.jpeg";
+import imageThree from "./IMG_3725.jpeg";
+import imageFour from "./IMG_4062.jpeg";
+import Video from "./Video.js";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="MoneyrookLogoHeader">
+        <img src={logoGif} alt="" length={190} width={190}></img>
+      </h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-6" id="BioSection">
+            <h2>About the artist</h2>
+            Biography section -Hometown -Age -Written in first-person or
+            third-person? -Facts/hobbies? -Projects, upcoming projects? Release
+            dates? -Possible merch?
+          </div>
+
+          <div className="col-6">
+            <div id="Images">
+              <div className="container">
+                <div className="row">
+                  <div className="col-3">
+                    <img src={imageOne} alt="" length={120} width={150}></img>
+                  </div>
+                  <div className="col-3">
+                    <img src={imageTwo} alt="" length={120} width={150}></img>
+                  </div>
+                  <div className="col-3">
+                    <img src={imageThree} alt="" length={120} width={150}></img>
+                  </div>
+                  <div className="col-3">
+                    <img src={imageFour} alt="" length={120} width={150}></img>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Video />
     </div>
   );
 }
