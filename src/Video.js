7e868d5793b137d385779e0e27e89f9645879ac3
tasialedet <87391755+tasialedet@users.noreply.github.com>
2021-12-06 18:04:47 -0600

    Added "Greatest" video link

--- src/Video.js
+++ src/Video.js
@@ -4,13 +4,13 @@ export default function Video() {
   return (
     <div className="YoutubeVideoPlayer">
       <iframe
-        id="player"
-        title="VideoPlayer"
-        type="text/html"
-        width="640"
-        height="390"
-        src="https://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/xyHFCQBcntA"
+        title="YouTube video player"
         frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
       ></iframe>
     </div>
   );
