eb4eddc2540d76d18947701b83963db6c1a805cf
tasialedet <87391755+tasialedet@users.noreply.github.com>
2021-12-06 18:00:11 -0600

    Added embedded Spotify to Music element

--- src/Music.js
+++ src/Music.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function Music() {
-  return <div className="Music">Hello from Music</div>;
+  return (
+    <div className="Music">
+      <iframe
+        title="musicPlayer"
+        src="https://open.spotify.com/embed/artist/7npJp99Ld8v36t6IVFLZW1?utm_source=generator"
+        width="100%"
+        height="380"
+        frameBorder="0"
+        allowfullscreen=""
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      ></iframe>
+    </div>
+  );
 }
